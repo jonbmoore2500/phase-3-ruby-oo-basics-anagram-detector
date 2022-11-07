a1c5afca34b717d8c44e033ed9e2b94cc863acad
Jonathan <jonathanmoorevla@gmail.com>
2022-11-06 21:00:50 -0600

    commit tree 5b0e0d1d5071ff4734ba8f917237092c2a33372e

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,23 @@
 # Your code goes here!
+require "pry"
+class Anagram
+    attr_accessor :word
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match(test_arr)
+        result_arr = []
+        
+        test_arr.each do |w|
+            if w.chars.sort == @word.chars.sort then
+                result_arr.push(w)
+            end
+        end
+        
+        result_arr
+    end
+end
+# binding.pry
+# 0
